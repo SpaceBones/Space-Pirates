ee8e4864ec7d8d9b4a0021d1940e0133d76023d4
Sean Ferreira <808seano@gmail.com>
2023-07-28 10:20:30 -1000

    New Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _speed = 10.0f;
+    //It's high because it's basically "how many degrees per second am I rotating?"
+    [SerializeField] private float _rotationSpeed = 250.0f;
     private float yBarrier = 6.55f;
     private float xBarrier = 11.28f;
 
@@ -19,6 +21,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        //AlternateMovement();
     }
 
     void CalculateMovement()
@@ -27,9 +30,11 @@ public class Player : MonoBehaviour
         float hInput = Input.GetAxis("Horizontal");
         float vInput = Input.GetAxis("Vertical");
         
-        transform.Translate(new Vector3(hInput, vInput, 0) * Time.deltaTime * _speed);
+        //The Top is forward and backward movement, while the Bottom is left and right rotation
+        transform.Translate(new Vector3(0, vInput, 0) * Time.deltaTime * _speed);
+        transform.Rotate(new Vector3(0, 0, hInput * -1) * Time.deltaTime * _rotationSpeed, Space.Self);
 
-        //if the current transform meets or exceeds the barrier, spawn them on the opposite side
+        //If the current transform meets or exceeds the barrier, spawn them on the opposite side
         if (transform.position.y >= yBarrier || transform.position.y <= (yBarrier * -1.0f))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y * -1.0f, transform.position.z);
@@ -38,5 +43,16 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x * -1.0f, transform.position.y, transform.position.z);
         }
+
+        //Boost Button?
     }
+
+// A scrapped idea that I really liked finding the answer to, so I left it in.
+//Basically; How to make the player follow the mouse. Could be used for an alternate control scheme
+    // void AlternateMovement()
+    // {
+    //     var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+    //     var angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg * -1.0f;
+    //     transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0,0,1));
+    // }
 }
