903c9201f05f0a24b391f92dfe4fb7bfb885bc36
Sean Ferreira <808seano@gmail.com>
2023-08-11 14:17:17 -1000

    Enemy Code Created

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,17 +7,13 @@ public class Laser : MonoBehaviour
     [SerializeField] private float _speed = 15.0f;
     private float yBarrier = 6.55f;
     private float xBarrier = 11.28f;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
         //translate laser up by a speed
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        //delete the laser if it goes full screen
         if (transform.position.y >= yBarrier || transform.position.y <= (yBarrier * -1.0f))
             Destroy(gameObject);
         if (transform.position.x >= xBarrier || transform.position.x <= (xBarrier * -1.0f))
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 10.0f;
+    [SerializeField] private float _boost = 12.0f;
     //It's high because it's basically "how many degrees per second am I rotating?"
     [SerializeField] private float _rotationSpeed = 250.0f;
     private float yBarrier = 6.55f;
     private float xBarrier = 11.28f;
     public GameObject laser;
     [SerializeField] private float _cooldown = 0.05f;
-    private float _canFire = 0.0f;
+    private float _canFire = 0.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -35,8 +36,11 @@ public class Player : MonoBehaviour
         float hInput = Input.GetAxis("Horizontal");
         float vInput = Input.GetAxis("Vertical");
         
-        //The Top is forward and backward movement, while the Bottom is left and right rotation
-        transform.Translate(new Vector3(0, vInput, 0) * Time.deltaTime * _speed);
+        //The Top is forward and backward movement in addition to boost, while the Bottom is left and right rotation
+        if (Input.GetKey(KeyCode.LeftShift) == true)
+            transform.Translate(new Vector3(0, vInput, 0) * Time.deltaTime * _boost);
+        else
+            transform.Translate(new Vector3(0, vInput, 0) * Time.deltaTime * _speed);
         transform.Rotate(new Vector3(0, 0, hInput * -1) * Time.deltaTime * _rotationSpeed, Space.Self);
 
         //If the current transform meets or exceeds the barrier, spawn them on the opposite side
@@ -45,7 +49,6 @@ public class Player : MonoBehaviour
         if (transform.position.x >= xBarrier || transform.position.x <= (xBarrier * -1.0f))
             transform.position = new Vector3(transform.position.x * -1.0f, transform.position.y, transform.position.z);
 
-        //Boost Button?
     }
 
 // A scrapped idea that I really liked finding the answer to, so I left it in.
