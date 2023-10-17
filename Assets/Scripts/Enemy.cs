3cb5ccb440cfd489971d928ddcbf222d9b1105e7
Sean Ferreira <808seano@gmail.com>
2023-10-17 09:42:06 -1000

    Added Filebase, Fixed Enemy Movement, Set up Sprites

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _speed = 4.0f;
+    [SerializeField] private float _speed = 3.0f;
     private float yBarrier = 6.55f;
     private float xBarrier = 11.28f;
+    //private float x_move = 0.0f;
+    //private float y_move = 0.0f;
     // private float[] _angles = {0.0f, 45.0f, 90.0f, 135.0f, 180.0f, -225.0f, 270.0f, 315.0f};
     // private int _angleCount = 8;
     // private int _tmpIndex = 0;
@@ -14,14 +16,31 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if you are on the positive end of the x axis AND your y value
+        // if(transform.position.x == 11.20f)
+        //     {
+        //         x_move = -1.0f;
+        //     }
+        // else if(transform.position.x == -11.20f)
+        //     {
+        //         x_move = 1.0f;
+        //     }
+        // else if(transform.position.y == 6.50f)
+        //     {
+        //         y_move = -1.0f;
+        //     }
+        // else if(transform.position.y == -6.50f)
+        //     {
+        //         y_move = 1.0f;
+        //     }
+        // Debug.Log("I have spawned at " + transform.position + " and have x_move value of " + x_move + " and a y_move value of " + y_move);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move in a random direction within an angle at 4 m/s
-        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+        //move in the chosen direction at 4 m/s
+        Movement();
         // **EXPERIMENTAL CODE**
         // if you touch the barrier, reappear on another random point on the barrier
         // if (Mathf.Abs(transform.position.y) >= yBarrier || Mathf.Abs(transform.position.x) >= xBarrier)
@@ -34,13 +53,18 @@ public class Enemy : MonoBehaviour
         //         _tmpAngle = _angles[_tmpIndex];
         //         this.transform.Rotate(0.0f, 0.0f, _tmpAngle, Space.Self);
         // }
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
         if (transform.position.y >= yBarrier || transform.position.y <= (yBarrier * -1.0f))
             transform.position = new Vector3(Random.Range(xBarrier * -1.0f, xBarrier), transform.position.y * -1.0f, transform.position.z);
         if (transform.position.x >= xBarrier || transform.position.x <= (xBarrier * -1.0f))
             transform.position = new Vector3(transform.position.x * -1.0f, Random.Range(yBarrier * -1.0f, yBarrier), transform.position.z);
     }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject newEnemy;
     [SerializeField] private GameObject _container;
     private bool _stopSpawning = false;
+    private string[] directions = {"north", "south", "east", "west"};
+    private Vector3 spawnPos = new Vector3(0.0f, 0.0f, 0.0f);
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +27,27 @@ public class SpawnManager : MonoBehaviour
         while (_stopSpawning == false)
         {
             yield return new WaitForSeconds(wait);
-            Vector3 spawnPos = new Vector3(Random.Range(-11.0f, 11.0f), 6.55f, 0);
-            GameObject newEnemy = Instantiate(_enemy, spawnPos, Quaternion.identity);
+            int x = Random.Range(0, 4);
+            if(directions[x] == "north")
+            {
+                spawnPos = new Vector3(Random.Range(-11.0f, 11.0f), 6.5f, 0);
+                newEnemy = Instantiate(_enemy, spawnPos, Quaternion.identity);
+            }
+            else if(directions[x] == "south")
+            {
+                spawnPos = new Vector3(Random.Range(-11.0f, 11.0f), -6.5f, 0);
+                newEnemy = Instantiate(_enemy, spawnPos, Quaternion.Euler(new Vector3(0.0f, 0.0f, 180.0f)));
+            }
+            else if (directions[x] == "east")
+            {
+                spawnPos = new Vector3(11.2f, Random.Range(-5.5f, 5.5f), 0);
+                newEnemy = Instantiate(_enemy, spawnPos, Quaternion.Euler(new Vector3(0.0f, 0.0f, 90.0f)));
+            }
+            else if (directions[x] == "west")
+            {
+                spawnPos = new Vector3(-11.2f, Random.Range(-5.5f, 5.5f), 0);
+                newEnemy = Instantiate(_enemy, spawnPos, Quaternion.Euler(new Vector3(0.0f, 0.0f, -90.0f)));
+            }
             newEnemy.transform.parent = _container.transform;
         }
 
