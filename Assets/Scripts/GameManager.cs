9f0e769f60ae7b0e318e65f6379779194d63103e
Sean Ferreira <808seano@gmail.com>
2023-11-27 12:06:31 -1000

    Added Main Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
 		{
-			SceneManager.LoadScene(0); //Current Game Scene
+			SceneManager.LoadScene(1); //Current Game Scene
 		}
 	}
 	public void GameOver()
