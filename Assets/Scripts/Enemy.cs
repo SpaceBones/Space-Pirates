8036bb2eb4539558aaa33def54c138a175d415cb
Sean Ferreira <808seano@gmail.com>
2023-11-28 10:58:27 -1000

    Added Death Animation for Enemy Units

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,63 +4,66 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3.0f;
-    private float _yBarrier = 6.55f;
-    private float _xBarrier = 11.28f;
-    [SerializeField] private Player _player;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
+	[SerializeField] private float _speed = 3.0f;
+	private float _yBarrier = 6.55f;
+	private float _xBarrier = 11.28f;
+	[SerializeField] private Player _player;
+	private Animator _animator;
+
+	// Start is called before the first frame update
+	void Start()
+	{
 		_player = GameObject.Find("Player").GetComponent<Player>();
+		_animator = GetComponent<Animator>();
 		if (_player == null)
-		{
-			Debug.LogError("The Player is Null!");
-		}
+			Debug.LogError("Custom Error: The Player is NULL!");
+		else if (_animator == null)
+			Debug.LogError("Custom Error: The Animator is NULL!");
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //move in the chosen direction at 4 m/s
-        Movement();
-        // **EXPERIMENTAL CODE**
-        // if you touch the barrier, reappear on another random point on the barrier
-        // if (Mathf.Abs(transform.position.y) >= yBarrier || Mathf.Abs(transform.position.x) >= xBarrier)
-        // {
-        //     if(Random.value < 0.5f) //Moving on the X Axis
-        //         transform.position = new Vector3(xBarrier * -1.0f, Random.Range(yBarrier * -1.0f, yBarrier), transform.position.z);
-        //     else //Moving on the Y Axis
-        //         transform.position = new Vector3(Random.Range(xBarrier * -1.0f, xBarrier), yBarrier, transform.position.z);
-        //     _tmpIndex = Random.Range(0, _angleCount);
-        //         _tmpAngle = _angles[_tmpIndex];
-        //         this.transform.Rotate(0.0f, 0.0f, _tmpAngle, Space.Self);
-        // }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		//move in the chosen direction at 4 m/s
+		Movement();
+		// **EXPERIMENTAL CODE**
+		// if you touch the barrier, reappear on another random point on the barrier
+		// if (Mathf.Abs(transform.position.y) >= yBarrier || Mathf.Abs(transform.position.x) >= xBarrier)
+		// {
+		//     if(Random.value < 0.5f) //Moving on the X Axis
+		//         transform.position = new Vector3(xBarrier * -1.0f, Random.Range(yBarrier * -1.0f, yBarrier), transform.position.z);
+		//     else //Moving on the Y Axis
+		//         transform.position = new Vector3(Random.Range(xBarrier * -1.0f, xBarrier), yBarrier, transform.position.z);
+		//     _tmpIndex = Random.Range(0, _angleCount);
+		//         _tmpAngle = _angles[_tmpIndex];
+		//         this.transform.Rotate(0.0f, 0.0f, _tmpAngle, Space.Self);
+		// }
+	}
 
-    private void Movement()
-    {
-        transform.Translate(Vector3.down * Time.deltaTime * _speed);
-        if (transform.position.y >= _yBarrier || transform.position.y <= (_yBarrier * -1.0f))
-            transform.position = new Vector3(Random.Range(_xBarrier * -1.0f, _xBarrier), transform.position.y * -1.0f, transform.position.z);
-        if (transform.position.x >= _xBarrier || transform.position.x <= (_xBarrier * -1.0f))
-            transform.position = new Vector3(transform.position.x * -1.0f, Random.Range(_yBarrier * -1.0f, _yBarrier), transform.position.z);
-    }
+	private void Movement()
+	{
+		transform.Translate(Vector3.down * Time.deltaTime * _speed);
+		if (transform.position.y >= _yBarrier || transform.position.y <= (_yBarrier * -1.0f))
+			transform.position = new Vector3(Random.Range(_xBarrier * -1.0f, _xBarrier), transform.position.y * -1.0f, transform.position.z);
+		if (transform.position.x >= _xBarrier || transform.position.x <= (_xBarrier * -1.0f))
+			transform.position = new Vector3(transform.position.x * -1.0f, Random.Range(_yBarrier * -1.0f, _yBarrier), transform.position.z);
+	}
 
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.tag == "Player")
-        {
-            Player player = other.transform.GetComponent<Player>();
-            if (player != null)
-                player.Damage();
-            Destroy(gameObject);
-        }
-        else if (other.tag == "Laser")
-        {
-            _player.AddScore(100);
-            Destroy(other.gameObject);
-            Destroy(gameObject);
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			Player player = other.transform.GetComponent<Player>();
+			if (player != null)
+				player.Damage();
+		}
+		else if (other.tag == "Laser")
+		{
+			_player.AddScore(100);
+			Destroy(other.gameObject);
+		}
+		_speed = 0.0f;
+		_animator.SetTrigger("On_Enemy_Death");
+		Destroy(gameObject, 2);
+	}
 }
